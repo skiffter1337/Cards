9d52ae6ea361b19f4759b72c89e2a4745b650cbd
Ilya <shulapov1999@gmail.com>
2023-06-29 16:50:01 +0300

    add checkbox stories

---
+++ src/components/ui/checkbox/checkbox.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { CheckboxItem } from './Checkbox.tsx'
+
+const meta = {
+  title: 'Components/Checkbox',
+  component: CheckboxItem,
+  tags: ['autodocs'],
+  argTypes: {
+    label: { control: 'text' },
+    disabled: { control: 'boolean' },
+    checked: { control: 'boolean' },
+    required: { control: 'boolean' },
+  },
+} satisfies Meta<typeof CheckboxItem>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const CheckboxDefault: Story = {
+  args: {},
+}
+export const CheckboxWithLabel: Story = {
+  args: {
+    label: 'Check me',
+  },
+}
