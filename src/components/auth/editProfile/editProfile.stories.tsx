deb8df7d54dc2dd11cf28f6bb1f0b296934e8b3d
Ilya <shulapov1999@gmail.com>
2023-07-15 16:47:33 +0300

    test

--- src/components/auth/editProfile/editProfile.stories.tsx
+++ src/components/auth/editProfile/editProfile.stories.tsx
@@ -1,18 +1,19 @@
-import { Meta, StoryObj } from '@storybook/react'
-
-import AvatarImg from '../../../images/png/avatar.png'
-
-import { EditProfile } from './editProfile.tsx'
-const meta = {
-  title: 'Auth/EditProfile',
-  component: EditProfile,
-  tags: ['autodocs'],
-  argTypes: {},
-} satisfies Meta<typeof EditProfile>
-
-export default meta
-type Story = StoryObj<typeof meta>
-
-export const Default: Story = {
-  args: { avatar: AvatarImg, email: 'j&johnson@gmail.com', userName: 'Ivan' },
-}
+// import { Meta, StoryObj } from '@storybook/react'
+//
+// import AvatarImg from '../../../images/png/avatar.png'
+//
+// import { EditProfile } from './editProfile.tsx'
+// const meta = {
+//   title: 'Auth/EditProfile',
+//   component: EditProfile,
+//   tags: ['autodocs'],
+//   argTypes: {},
+// } satisfies Meta<typeof EditProfile>
+//
+// export default meta
+// type Story = StoryObj<typeof meta>
+//
+// export const Default: Story = {
+//   args: { avatar: AvatarImg, email: 'j&johnson@gmail.com', userName: 'Ivan' },
+// }
+export default ''
