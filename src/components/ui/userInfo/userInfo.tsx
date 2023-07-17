5b9c0d325c40ea8bf92c290317d61ff6d0299735
Ilya <shulapov1999@gmail.com>
2023-07-17 10:30:57 +0300

    fix dropdown

---
+++ src/components/ui/userInfo/userInfo.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react'
+
+import { Avatar } from '../../header/avatar'
+import { Typography } from '../typography'
+
+import s from './../../ui/dropdown/dropdown.module.scss'
+
+type UserInfoPropsType = {
+  userName: string
+  src: string
+}
+
+export const UserInfo: FC<UserInfoPropsType> = ({ userName, src }) => {
+  return (
+    <>
+      <Typography variant={'subtitle1'} className={s.user_name}>
+        {userName}
+      </Typography>
+      <Avatar src={src} />
+    </>
+  )
+}
